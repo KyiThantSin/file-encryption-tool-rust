ba76ef5a0e0d228510255555ba6db9f440f0a487
KyiThantSin <kyits485@gmail.com>
2024-10-09 21:31:36 +0700

    update-encryption

--- src/crypto/chacha20.rs
+++ src/crypto/chacha20.rs
@@ -1,10 +1,11 @@
 use chacha20::cipher::{KeyIvInit, StreamCipher};
 use chacha20::ChaCha20;
 use rand::Rng;
+use sha2::{Sha256, Digest};
 use std::fs::{File, OpenOptions};
 use std::io::{self, Read, Write};
 use std::path::Path;
-
+use hex;
 struct Sizes;
 
 impl Sizes {
@@ -23,22 +24,21 @@ impl Sizes {
     }
 }
 
-pub fn encrypt_file<T: AsRef<Path>>(file_path: T) -> Result<([u8; 32], [u8; 12]), std::io::Error> {
+pub fn encrypt_file<T: AsRef<Path>>(file_path: T) -> Result<(String, String), std::io::Error> {
     let sizes = Sizes::new();
     let (key, nonce) = sizes.generate();
 
-    // Open 
+    // Open the file
     let mut file = File::open(file_path.as_ref())?;
-    let mut data = Vec::new(); // read into a Vec<u8>
-
+    let mut data = Vec::new(); // Read into a Vec<u8>
     file.read_to_end(&mut data)?;
 
     // ChaCha20 cipher instance
     let mut cipher = ChaCha20::new(&key.into(), &nonce.into());
     cipher.apply_keystream(&mut data);
-    // println!("Encrypted Data: {:?}", data);
-    
-    std::fs::create_dir_all("testings")?; // create if it does not exist
+
+    // Create directory if it does not exist
+    std::fs::create_dir_all("testings")?;
 
     let mut file = OpenOptions::new()
         .write(true)
@@ -49,7 +49,20 @@ pub fn encrypt_file<T: AsRef<Path>>(file_path: T) -> Result<([u8; 32], [u8; 12])
     file.write_all(&data)?;
     println!("Encrypted data written to file.");
 
-    Ok((key,nonce))
+    // Hash key and nonce using SHA-256
+    let mut hasher = Sha256::new();
+    hasher.update(&key);
+    let key_hash = hasher.finalize();
+    
+    let mut hasher_nonce = Sha256::new();
+    hasher_nonce.update(&nonce);
+    let nonce_hash = hasher_nonce.finalize();
+
+    // Convert the hashed values to hexadecimal strings
+    let key_hex = hex::encode(key_hash);
+    let nonce_hex = hex::encode(nonce_hash);
+
+    Ok((key_hex, nonce_hex))
 }
 
 pub fn decrypt_file<T: AsRef<Path>>(file_path: T) -> Result<(), std::io::Error>{
